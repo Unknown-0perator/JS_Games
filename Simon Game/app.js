7404e82be87c2c9b97279755e6aebaac89d6e5c5
Ahmad Rashid Akhtar <ahmadrashidakhtar@gmail.com>
2022-08-01 21:35:58 -0700

    user click event listener created

--- Simon Game/app.js
+++ Simon Game/app.js
@@ -1,7 +1,16 @@
 "use strict";
+
 const gamePattern = [];
 const btnColors = ["red", "blue", "green", "yellow"];
+const userClickedPattern = [];
+let classObj = document.getElementsByClassName("btn");
 
+for (let i = 0; i < classObj.length; i++) {
+  classObj[i].addEventListener("click", function (e) {
+    let elementId = e.target.id;
+    userClickedPattern.push(elementId);
+  });
+}
 const fadeIn = function (id) {
   const fade = document.getElementById(id);
   let opacity = 0;
@@ -19,7 +28,10 @@ const nextSequence = function () {
   let randomNumber = Math.trunc(Math.random() * 4);
   let randomColorChosen = btnColors[randomNumber];
   gamePattern.push(randomColorChosen);
-  let randomCOlor = document.getElementById(randomColorChosen).id;
-  fadeIn(randomCOlor);
+  let randomColorID = document.getElementById(randomColorChosen).id;
+  fadeIn(randomColorID);
+  var audio = new Audio(`sounds/${randomColorID}.mp3`);
+  audio.play;
 };
+
 nextSequence();
