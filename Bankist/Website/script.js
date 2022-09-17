ec1fcbd701ab8c9eb037859c2d48d5ee6eb02e5d
Ahmad Rashid Akhtar <ahmadrashidakhtar@gmail.com>
2022-09-16 20:32:41 -0700

    cookies pop up added

--- Bankist/Website/script.js
+++ Bankist/Website/script.js
@@ -26,3 +26,19 @@ document.addEventListener("keydown", function (e) {
     closeModal();
   }
 });
+const header = document.querySelector(".header");
+const message = document.createElement("div");
+message.classList.add("cookie-message");
+message.innerHTML =
+  "We are using cookies for fast reloading <button class = 'btn btn--close-cookie'>Accept</button>";
+header.append(message);
+
+document
+  .querySelector(".btn--close-cookie")
+  .addEventListener("click", function () {
+    message.remove();
+  });
+
+message.style.backgroundColor = "#37383d";
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + "px";
