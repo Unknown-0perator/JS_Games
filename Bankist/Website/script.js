1b3e50456f5254e1e5cdda70bf12b367b5f4dfec
Ahmad Rashid Akhtar <ahmadrashidakhtar@gmail.com>
2022-09-19 15:09:02 -0700

    Smooth page navigation added using event delegation

--- Bankist/Website/script.js
+++ Bankist/Website/script.js
@@ -47,3 +47,23 @@ const section1 = document.querySelector("#section--1");
 btnScrollTo.addEventListener("click", function (e) {
   section1.scrollIntoView({ behavior: "smooth" });
 });
+
+//////////////////////
+// Page navigation
+
+// document.querySelectorAll(".nav__link").forEach(function (element) {
+//   element.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute("href");
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+//   });
+// });
+
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
