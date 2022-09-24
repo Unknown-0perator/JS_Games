d53522bf3ac45cd50d25b7d2c0639bcd649fd646
Ahmad Rashid Akhtar <ahmadrashidakhtar@gmail.com>
2022-09-23 20:39:27 -0700

    code refactoring

--- Bankist/Website/script.js
+++ Bankist/Website/script.js
@@ -28,68 +28,6 @@ const closeModal = function () {
   overlay.classList.add("hidden");
 };
 
-btnsOpenModal.forEach((btn) => btn.addEventListener("click", openModal));
-
-btnCloseModal.addEventListener("click", closeModal);
-overlay.addEventListener("click", closeModal);
-
-document.addEventListener("keydown", function (e) {
-  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
-    closeModal();
-  }
-});
-
-message.classList.add("cookie-message");
-message.innerHTML =
-  "We are using cookies for fast reloading <button class = 'btn btn--close-cookie'>Accept</button>";
-header.append(message);
-
-document
-  .querySelector(".btn--close-cookie")
-  .addEventListener("click", function () {
-    message.remove();
-  });
-message.style.height =
-  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + "px";
-
-btnScrollTo.addEventListener("click", function (e) {
-  section1.scrollIntoView({ behavior: "smooth" });
-});
-
-//////////////////////
-// Page navigation
-
-// document.querySelectorAll(".nav__link").forEach(function (element) {
-//   element.addEventListener("click", function (e) {
-//     e.preventDefault();
-//     const id = this.getAttribute("href");
-//     console.log(id);
-//     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
-//   });
-// });
-
-document.querySelector(".nav__links").addEventListener("click", function (e) {
-  e.preventDefault();
-  if (e.target.classList.contains("nav__link")) {
-    const id = e.target.getAttribute("href");
-    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
-  }
-});
-// Tabbed component
-
-tabsContainer.addEventListener("click", function (e) {
-  const clicked = e.target.closest(".operations__tab");
-  if (!clicked) return;
-  tabs.forEach((t) => t.classList.remove("operations__tab--active"));
-  clicked.classList.add("operations__tab--active");
-  tabsContent.forEach((tc) =>
-    tc.classList.remove("operations__content--active")
-  );
-  document
-    .querySelector(`.operations__content--${clicked.dataset.tab}`)
-    .classList.add("operations__content--active");
-});
-
 const handleHover = function (e) {
   if (e.target.classList.contains("nav__link")) {
     const link = e.target;
@@ -104,57 +42,6 @@ const handleHover = function (e) {
     logo.style.opacity = this;
   }
 };
-
-nav.addEventListener("mouseover", handleHover.bind(0.5));
-nav.addEventListener("mouseout", handleHover.bind(1));
-
-const stickyNav = function (entries) {
-  const [entry] = entries;
-  if (!entry.isIntersecting) nav.classList.add("sticky");
-  else nav.classList.remove("sticky");
-};
-const navHeight = nav.getBoundingClientRect().height;
-const headerObserver = new IntersectionObserver(stickyNav, {
-  root: null,
-  threshold: 0.15,
-  rootMargin: `-${navHeight}px`,
-});
-
-headerObserver.observe(header);
-
-const allSections = document.querySelectorAll(".section");
-const revealSection = function (entries, observe) {
-  const [entry] = entries;
-  if (!entry.isIntersecting) return;
-  entry.target.classList.remove("section--hidden");
-  observe.unobserve(entry.target);
-};
-
-const sectionObserver = new IntersectionObserver(revealSection, {
-  root: null,
-  threshold: 0.15,
-});
-allSections.forEach(function (section) {
-  sectionObserver.observe(section);
-  section.classList.add("section--hidden");
-});
-
-const imageTargets = document.querySelectorAll("img[data-src]");
-const loadImg = function (entries, observer) {
-  const [entry] = entries;
-  if (!entry.isIntersecting) return;
-  entry.target.src = entry.target.dataset.src;
-  entry.target.addEventListener("load", function () {
-    entry.target.classList.remove("lazy-img");
-  });
-  observer.unobserve(entry.target);
-};
-const imageObserver = new IntersectionObserver(loadImg, {
-  root: null,
-  threshold: 0,
-  rootMargin: "200px",
-});
-imageTargets.forEach((img) => imageObserver.observe(img));
 
 const createDots = function () {
   slides.forEach(function (_, i) {
@@ -183,10 +70,7 @@ const goToSlide = function (slide) {
   );
   activateDot(slide);
 };
-let currentSlide = 0;
-let maxSlide = slides.length;
-createDots();
-goToSlide(0);
+
 const nextSlide = function () {
   if (currentSlide === maxSlide - 1) {
     currentSlide = 0;
@@ -203,6 +87,99 @@ const perviousSlide = function () {
   }
   goToSlide(currentSlide);
 };
+
+btnsOpenModal.forEach((btn) => btn.addEventListener("click", openModal));
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
+message.classList.add("cookie-message");
+message.innerHTML =
+  "We are using cookies for fast reloading <button class = 'btn btn--close-cookie'>Accept</button>";
+header.append(message);
+document
+  .querySelector(".btn--close-cookie")
+  .addEventListener("click", function () {
+    message.remove();
+  });
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + "px";
+btnScrollTo.addEventListener("click", function (e) {
+  section1.scrollIntoView({ behavior: "smooth" });
+});
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
+tabsContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".operations__tab");
+  if (!clicked) return;
+  tabs.forEach((t) => t.classList.remove("operations__tab--active"));
+  clicked.classList.add("operations__tab--active");
+  tabsContent.forEach((tc) =>
+    tc.classList.remove("operations__content--active")
+  );
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add("operations__content--active");
+});
+nav.addEventListener("mouseover", handleHover.bind(0.5));
+nav.addEventListener("mouseout", handleHover.bind(1));
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) nav.classList.add("sticky");
+  else nav.classList.remove("sticky");
+};
+const navHeight = nav.getBoundingClientRect().height;
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0.15,
+  rootMargin: `-${navHeight}px`,
+});
+headerObserver.observe(header);
+const allSections = document.querySelectorAll(".section");
+const revealSection = function (entries, observe) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("section--hidden");
+  observe.unobserve(entry.target);
+};
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
+const imageTargets = document.querySelectorAll("img[data-src]");
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.src = entry.target.dataset.src;
+  entry.target.addEventListener("load", function () {
+    entry.target.classList.remove("lazy-img");
+  });
+  observer.unobserve(entry.target);
+};
+const imageObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: "200px",
+});
+imageTargets.forEach((img) => imageObserver.observe(img));
+
+let currentSlide = 0;
+let maxSlide = slides.length;
+createDots();
+goToSlide(0);
+
 btnRight.addEventListener("click", nextSlide);
 btnLeft.addEventListener("click", perviousSlide);
 document.addEventListener("keydown", function (e) {
