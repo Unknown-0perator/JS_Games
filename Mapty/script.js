83aa70ad7d3c5700df34f9df38f54b4ac9f8a39e
Ahmad Rashid Akhtar <ahmadrashidakhtar@gmail.com>
2022-10-03 18:40:38 -0700

    Map added to the application using leaflet and current location marked using the geolocation

--- Mapty/script.js
+++ Mapty/script.js
@@ -1,7 +1,19 @@
 "use strict";
 
-// prettier-ignore
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
 const form = document.querySelector(".form");
 const containerWorkouts = document.querySelector(".workouts");
@@ -10,3 +22,23 @@ const inputDistance = document.querySelector(".form__input--distance");
 const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      const coord = [latitude, longitude];
+      const map = L.map("map").setView(coord, 15);
+      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        maxZoom: 19,
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      }).addTo(map);
+      const marker = L.marker(coord).addTo(map);
+    },
+    function () {
+      alert("Faild to get your position");
+    }
+  );
+}
