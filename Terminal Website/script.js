b90373c9bb4874896211f1979c5d54fad6c37412
Ahmad Rashid Akhtar <ahmadrashidakhtar@gmail.com>
2022-10-25 19:56:17 -0700

    Commands created

---
+++ Terminal Website/script.js
@@ -0,0 +1,28 @@
+const linkedin = "https://www.linkedin.com/in/ahmadrashidakhtar/";
+const github = "https://github.com/Unknown-0perator";
+
+about = [
+  "<br>",
+  "Hello World, I am Ahmad Rashid Akhtar",
+  "I'm a software developer",
+  "<br>",
+];
+
+social = [
+  "<br>",
+  'linkedin       <a href="' +
+    linkedin +
+    '">linkedin/ahmadrashidakhtar' +
+    "</a>",
+  'github         <a href="' + github + '" >github/unknown-0perator' + "</a>",
+  "<br>",
+];
+
+help = [
+  "<br>",
+  '<span class="command">about</span>          About me',
+  '<span class="command">contact</span>        Contact me',
+  '<span class="command">help</span>           Help',
+  '<span class="command">clear</span>          Clear terminal',
+  "<br>",
+];
